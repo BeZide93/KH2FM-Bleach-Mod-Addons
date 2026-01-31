bf91f9776cb3562f0d40d746a00a407b85ac0c9e
BeZide93 <r.wolter93@gmx.de>
2026-01-31 23:31:40 +0100

    Add files via upload

--- scripts/GoAWarp.lua
+++ scripts/GoAWarp.lua
@@ -1,5 +1,5 @@
-LUAGUI_NAME = "Enable GoA Warp (Steam)"
-LUAGUI_AUTH = "BeZide"
+LUAGUI_NAME = "Enable GoA Warp (Steam) - PS2 Equivalent"
+LUAGUI_AUTH = "ported from pnach"
 LUAGUI_DESC = "Enable GoA Warp (Index 55) only if GardenKnown-Flag == 0x07 (Steam GL)."
 
 stmcheck = 0x585E59
@@ -9,8 +9,7 @@ local SAVEDATA_BASE = 0x9A98B0
 
 local OFF_GARDEN_FLAG = 0x231B
 local OFF_GOA_WARP    = 0x1EF6
-:
--- If GardenFlag == 0x07 then write 0x7F to warp index address
+
 local REQUIRED_GARDEN = 0x07
 local WARP_VALUE      = 0x7F
 
@@ -37,6 +36,13 @@ function _OnFrame()
 
     local garden = ReadByte(SAVEDATA_BASE + OFF_GARDEN_FLAG)
     local warp   = ReadByte(SAVEDATA_BASE + OFF_GOA_WARP)
+
+    -- Optional:
+    lastPrint = lastPrint + 1
+    if lastPrint >= 120 then
+        lastPrint = 0
+        ConsolePrint(string.format("[GoA Warp] garden=%02X warp=%02X", garden, warp))
+    end
 
     if done then return end
 
